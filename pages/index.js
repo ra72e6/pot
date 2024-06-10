02e4805590561151ebf278c7c1a0aef54626e49b
ra72e6 <ra72e6@hanmail.net>
2024-06-10 18:30:33 +0900

    3st

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,40 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 import Image from 'next/image';
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    const imgElement = document.getElementById('clickableImage');
+    if (imgElement) {
+      imgElement.addEventListener('click', handleClick);
+    }
+    // Cleanup the event listener on component unmount
+    return () => {
+      if (imgElement) {
+        imgElement.removeEventListener('click', handleClick);
+      }
+    };
+  }, []);
+
+  const handleClick = () => {
+    alert('이미지를 클릭하였습니다');
+  };
+
   return (
     <div className={styles.container}>
       <Head>
-        <title> 홈</title>
+        <title>홈</title>
       </Head>
 
       <h1 className={styles.homeTitle}>안녕하세요.</h1>
-      <Image src="/images/hellokitty.gif" width={500} height={500}></Image>
+      <Image
+        id="clickableImage"
+        src="/images/hellokitty.gif"
+        width={500}
+        height={500}
+        style={{ cursor: 'pointer' }}
+      />
     </div>
   );
 }
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,10 +1,18 @@
 export default function ProfileDefault() {
+  const profileData = [
+    { label: '이름', value: '임혜준' },
+    { label: '나이', value: '21살' },
+    { label: '생일', value: '2002년 10월 25일' },
+  ];
+
   return (
     <div>
       <h1>/ᐠ.ꞈ.ᐟ\</h1>
-      <p> 이름 : 임혜준 </p>
-      <p> 나이 : 21살 </p>
-      <p> 생일 : 2002년 10월 25일 </p>
+      {profileData.map((item, index) => (
+        <p key={index}>
+          {item.label} : {item.value}
+        </p>
+      ))}
     </div>
   );
 }
