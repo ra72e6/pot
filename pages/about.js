83c9fb1f96981e2c38d07a3776167ed27807eb0f
ra72e6 <ra72e6@hanmail.net>
2024-06-13 22:18:08 +0900

    0613st

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,30 @@
+import Link from 'next/link';
+import React from 'react';
+import Image from 'next/image';
+
 export default function About() {
   return (
     <div>
-      <h2>리엑트</h2>
-      <a href="react-3m6ej2jtq-ra72e6s-projects.vercel.app">리엑트</a>
-      <h2>버셀</h2>
-      <a href="https://vercel.com/ra72e6s-projects">버셀</a>
-      <h2>깃허브</h2>
-      <a href="https://github.com/ra72e6">깃허브</a>
+      <h1 className="text-2xl font-bold mb-8">포트폴리오</h1>
+
+      <div className="bg-gray-200 border border-slate-400 px-4 py-2 rounded-md mb-4">
+        <h1 className="text-lg font-bold">BookSite</h1>
+        <Image src="/images/booksite.jpg" width={300} height={300}></Image>
+        <Link href="https://book-ra72e6.vercel.app/" className="text-green-800">
+          link
+        </Link>
+      </div>
+      <div className="bg-gray-200 border border-slate-400 px-4 py-2 rounded-md mb-4">
+        <h1 className="text-lg font-bold">Nextmall</h1>
+        <Image src="/images/nextmall.jpg" width={300} height={300}></Image>
+        <br />
+        <Link
+          href="https://nextmall1-nco4tkx7b-ra72e6.vercel.app/"
+          className="text-green-800"
+        >
+          link
+        </Link>
+      </div>
     </div>
   );
 }
